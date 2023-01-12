70d708bff531f82c3835c20dba229c6698080f6d
Elineia Jessica <elineiajsmendonca@gmail.com>
2023-01-12 15:19:54 -0300

    changes in code with sync to API and my onw model

--- Pokedex-JS/assets/js/main.js
+++ Pokedex-JS/assets/js/main.js
@@ -1,67 +1,24 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-        <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
     <div class="detail">
         <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}            
         </ol>
 
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        <img src="${pokemon.photo}" 
+            alt="${pokemon.name}">
 
     </div>
     </li>`
 }
 
-const pokemonList = document.getElementById('pokemonList')
+const pokemonList = document.getElementById('pokemonList');
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        console.log(pokemons);
-
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-
-        }
-
-    })
-    .catch((error) => console.log(error))
-
-
-
-
-
-{/* <li>
-                Ivysaur
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg" alt="Ivysaur">
-
-            </li>
-            <li>
-                Venusaur
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg" alt="Venusaur">
-
-            </li>
-            <li>
-                Charmander
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg" alt="Charmander">
-
-            </li>
-            <li>
-                Charmeleon
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg" alt="Charmeleon">
-
-            </li>
-            <li>
-                Charizard
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg" alt="Charizard">
-
-            </li> */}
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
+//Acima está havendo o mapeamento dos pokemons junto a lista usando o método join e, antes disso, está concatenando ao html que já tinha
